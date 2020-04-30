6625636a5c80217eb7bae25780aa2e582324fd7f
Brendan <ngoranbren@gmail.com>
2020-04-29 22:14:26 -0500

    dockerfiles added

--- Calm/src/app/app.module.ts
+++ Calm/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { UserLoginComponent } from './user-login/user-login.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { RegisterComponent } from './register/register.component';
+import { LogOutComponent } from './log-out/log-out.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //LoginComponent,
+    UserLoginComponent,
+    RegisterComponent,
+    LogOutComponent
   ],
   imports: [
     BrowserModule,
