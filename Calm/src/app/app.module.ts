f007b3f67365d8161c7a723397e41eb94329c5c5
Brendan <ngoranbren@gmail.com>
2020-04-30 20:08:17 -0500

    dockercompose completed

--- Calm/src/app/app.module.ts
+++ Calm/src/app/app.module.ts
@@ -3,24 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//import { UserLoginComponent } from './user-login/user-login.component';
-import { UserLoginComponent } from './user-login/user-login.component';
-import { RegisterComponent } from './register/register.component';
-import { LogOutComponent } from './log-out/log-out.component';
 
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     //LoginComponent,
-//     UserLoginComponent,
-//     RegisterComponent,
-//     LogOutComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-export class AppModule { }
+
+
+ @NgModule({
+  declarations: [
+   AppComponent,
+     ],
+  imports: [
+     BrowserModule,
+    AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
+ }) export class AppModule { }
