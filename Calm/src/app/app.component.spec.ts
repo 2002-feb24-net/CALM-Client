465f044934e2fe3358b771c30993f301aab88d38
kesh24 <keshnarana4@gmail.com>
2020-05-01 09:46:16 -0500

    responsive css

--- Calm/src/app/app.component.spec.ts
+++ Calm/src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Calm');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Calm app is running!');
-  });
+ 
 });
--- Calm/src/app/questions/questions.component.ts
+++ Calm/src/app/questions/questions.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-questions',
@@ -12,20 +12,13 @@ export class QuestionsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  Submit(f: NgForm) {
-    // let element = <HTMLInputElement> document.getElementById("q1");  
-    // let element2 = <HTMLInputElement> document.getElementById("q2"); 
-    // let element3 = <HTMLInputElement> document.getElementById("q3");
-    // let element4 = <HTMLInputElement> document.getElementById("q4");
-    // let element5 = <HTMLInputElement> document.getElementById("q5"); 
-    // let element6 = <HTMLInputElement> document.getElementById("q6");        
-    // if (element.checked) {
+  Submit() {
+    
       document.getElementById('p').innerText= ("Recommended groups: ")
     
      for(let i=1; i<=6; i++ )
      {
     var element= <HTMLInputElement> document.getElementById(`${i}`);
-     console.log(element); 
      if (element.checked) {
       document.getElementById('p').innerText += element.value;
       }
