149571b44bd0e44b436e42b0bc58739e7da0c095
kesh24 <keshnarana4@gmail.com>
2020-05-01 08:27:31 -0500

    questions

--- Calm/src/app/questions/questions.component.ts
+++ Calm/src/app/questions/questions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-questions',
@@ -10,6 +11,25 @@ export class QuestionsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  Submit(f: NgForm) {
+    // let element = <HTMLInputElement> document.getElementById("q1");  
+    // let element2 = <HTMLInputElement> document.getElementById("q2"); 
+    // let element3 = <HTMLInputElement> document.getElementById("q3");
+    // let element4 = <HTMLInputElement> document.getElementById("q4");
+    // let element5 = <HTMLInputElement> document.getElementById("q5"); 
+    // let element6 = <HTMLInputElement> document.getElementById("q6");        
+    // if (element.checked) {
+      document.getElementById('p').innerText= ("Recommended groups: ")
+    
+     for(let i=1; i<=6; i++ )
+     {
+    var element= <HTMLInputElement> document.getElementById(`${i}`);
+     console.log(element); 
+     if (element.checked) {
+      document.getElementById('p').innerText += element.value;
+      }
+     }
   }
 
 }
