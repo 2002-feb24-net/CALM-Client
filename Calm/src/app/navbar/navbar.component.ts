cf5faf53780e87ba5d87d5433d86719cdb5c7062
kesh24 <keshnarana4@gmail.com>
2020-05-02 21:03:33 -0500

    missing nav

--- Calm/src/app/navbar/navbar.component.ts
+++ Calm/src/app/navbar/navbar.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-/**
- * Navigation component
- */
+import { CookieService } from 'ngx-cookie-service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-/**
- * @ignore
- */
-  constructor() { }
-/**
- * @ignore
- */
-  ngOnInit(): void {
-  }
+
+  constructor(
+    private cookieService: CookieService
+  ) { }
+
+
+
+    data=this.cookieService.get('Username');
+    show: boolean = false;
+   ngOnInit(): void {
+ 
+     this.show = false;
+    if(this.data != ""){
+ this.show = true;
+    }
+    else
+    this.show = false;
+   }
+ 
+  
 
 }
