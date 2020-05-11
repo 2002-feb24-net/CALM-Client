7d2c6102ef32b5fd11aceb5fd250993a4cb1c2a0
kesh24 <keshnarana4@gmail.com>
2020-05-11 18:11:51 -0500

    only show no edit yet

--- Calm/src/app/support-groups/support-groups.component.ts
+++ Calm/src/app/support-groups/support-groups.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { CookieService } from 'ngx-cookie-service';
 import { UserService } from '../services/user.service';
+import { EventsService } from '../services/events.service';
 
 /**
  * Support group component
@@ -15,6 +16,7 @@ export class SupportGroupsComponent implements OnInit {
  * @ignore
  */
   constructor(public LoginService:UserService,
+    public eventApi:EventsService,
     private cookieService: CookieService) { }
    data=this.cookieService.get('username');
    show: boolean = false;
@@ -23,7 +25,7 @@ export class SupportGroupsComponent implements OnInit {
  * @ignore
  */
   ngOnInit(): void {
-
+    this.eventApi.refreshgroups();
     this.show = false;
    if(this.data != ""){
 this.show = true;
